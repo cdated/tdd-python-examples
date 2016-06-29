6e537d20451d40c73c32918ec151b495cadf704f
Brandon T. Fields <brandon@cdated.com>
2016-06-28 22:34:12 -0400

    Updates firefox bin path, adds user story, updates gitignore

--- functional_tests.py
+++ functional_tests.py
@@ -7,21 +7,14 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 caps = DesiredCapabilities.FIREFOX
 
 # Tell the Python bindings to use Marionette.
-# This will not be necessary in the future,
-# when Selenium will auto-detect what remote end
-# it is talking to.
 caps["marionette"] = True
 
 # Path to Firefox DevEdition or Nightly.
-# Firefox 47 (stable) is currently not supported,
-# and may give you a suboptimal experience.
-#
-# On Mac OS you must point to the binary executable
-# inside the application package, such as
-# /Applications/FirefoxNightly.app/Contents/MacOS/firefox-bin
-caps["binary"] = "/Applications/Firefox.app/Contents/MacOS/firefox-bin"
+caps["binary"] = "/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox-bin"
 
+# Edith heard about an online to-do app, she goes to the site
 browser = webdriver.Firefox(capabilities=caps)
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the pages title and header mention to-do lists
+assert 'To-Do' in browser.title
