c3b03ff48fc3f12c1aeb46ee4d0f6eb31fb03d06
Brandon T. Fields <brandon@cdated.com>
2016-07-04 22:24:15 -0400

    Refactor check item in table

--- functional_tests.py
+++ functional_tests.py
@@ -27,6 +27,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith heard about an online to-do app, she goes to the site
         self.browser.get('http://localhost:8000')
@@ -57,12 +62,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',
-                [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
