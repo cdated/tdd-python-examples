7dc0d83de618568a899bd3c718bd70495763c637
Brandon T. Fields <brandon@cdated.com>
2016-07-05 21:38:22 -0400

    Enumerate to-do items

--- functional_tests.py
+++ functional_tests.py
@@ -58,7 +58,6 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box invitingher to add another item.
         # She enters "Use peacock feathers to make a fly"
-        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
