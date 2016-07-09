fa053ccc84150e4a6af44ea24ca2ab408c229578
Brandon T. Fields <brandon@cdated.com>
2016-07-09 19:22:03 -0400

    Add functional tests for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -113,11 +113,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-
-
-        self.fail('Finish the test!')
-
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees the input is nicely centered too
+        inputbox.send_keys('testing\n')
+        inputbox.send_keys(Keys.ENTER)
+        # Pause for redirect
+        time.sleep(1)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
