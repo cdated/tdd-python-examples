16a42a8babc7095fe3b9439bac7900df0dd4670b
Brandon T. Fields <brandon@cdated.com>
2016-07-10 00:04:15 -0400

    Disable admin in settings installed applications

--- superlists/settings.py
+++ superlists/settings.py
@@ -42,7 +42,7 @@ TEMPLATES = [
 ]
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
