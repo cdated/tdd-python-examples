064e10062c11af967a47b1e64952ccda21d48db0
Brandon T. Fields <brandon@cdated.com>
2016-07-23 19:42:17 -0400

    Fix bug in live functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -32,6 +32,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         for arg in sys.argv:
             if 'liveserver' in arg:
                 cls.server_url = 'http://' + arg.split('=')[1]
+                cls.live_server_url = cls.server_url
                 return
         super().setUpClass()
         cls.server_url = cls.live_server_url
